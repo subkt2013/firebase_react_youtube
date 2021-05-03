ba2f83ccac704a8a70e87426d35d0f38fa81a165
subkt2013 <subkt2013@gmail.com>
2021-05-03 22:11:28 +0900

    テスト

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://jirou-narabi.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ワイがテストで作ったサイト
         </a>
       </header>
     </div>
